c6846d34176659c9b1eb9e93775e73f9e013af67
Skyler Daché <skyberskyman@gmail.com>
2018-07-09 18:28:17 -0400

    added stuff

---
+++ challenge.py
@@ -0,0 +1,38 @@
+import typing
+import matplotlib.pyplot as plt
+import numpy as np
+
+def question1() -> None:
+    """ Question 1:
+    What is the mean (total) pledge that projects get? (not per backer)*
+    Your answer may vary by +/- 5%
+    """
+    pass
+def question2a() -> None:
+    """ Question 2:
+    Create a histogram that shows the distribution for number of backers.
+    
+    """
+    pass
+def question2b() -> None:
+    """
+    What is the skew of the distribution?*
+    """
+    pass
+def question3a() -> None:
+    """Question 3:
+    Is the ‘duration’ variable normally distributed?*
+    """
+    pass
+def question3b() -> None:
+    """
+    If you could collect data on another attribute of these projects, what would it be and why?*
+    """
+    print("Hello world!")
+def main() -> None:
+    question1()
+    question2a()
+    question2b()
+    question3a()
+    question3b()
+main()
